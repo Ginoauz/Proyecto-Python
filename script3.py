99105f97fab0dce294c02a51cf012e173511ab62
Ginoauz <92491634+Ginoauz@users.noreply.github.com>
2021-10-13 22:54:05 -0500

    Add files via upload

---
+++ script3.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 13 20:14:29 2021
+
+@author: Marcel
+"""
+# imprime el dato de trabajar con python
+print("Empezando a trabajar con Python")
+#imprime mi nombre
+print("Realizado por: Gino Auz")
+#imprime lo que se va a realizar
+print("Consultando los tipos de valores:")
+#imprime el tipo de dato entero
+print("El tipo de dato de 875 es:", type(845))
+#imprime el tipo de dato decimal
+print("El tipo de dato de 4.89 es:", type(4.89))
+#imprime el tipo de dato string
+print("El tipo de dato del texto: Now is better than never. es:",type("Now is better than never"))
+#imprime el tipo de dato decimal
+print("El tipo de dato de 1.32 es:", type(1.32))
+#imprime falso al tipo de dato que no es entero
+print("¿El valor 5 + 8i corresponde a un valor entero?", isinstance(5+8j,int))
+#imprime falso porque el dato no es entero
+print("¿El valor 8.2 corresponde a un valor entero?", isinstance(8.2,int))
+#imprime verdadero al dato string
+print("¿El texto: Readability counts. corresponde a un string?", isinstance("Readability counts", str))
+
+
+
+      
+      
